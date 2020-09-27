51dbd67c8b315d8c83bdd4c70fae73ebce6d5033
Hhpon <624791164@qq.com>
2020-09-27 23:02:50 +0800

    modify port

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,4 +1,4 @@
-export const HOST = 'http://47.94.210.206:3007'  // http://47.94.210.206
+export const HOST = ''  // http://47.94.210.206
 export const ERR_OK = 200
 
 export const playMode = {
--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-import 'babel-polyfill'
-import Vue from 'vue'
-import './cube-ui'
-import App from './App.vue'
-import router from './router'
-import store from './vuex/store'
-import 'common/scss/index.scss'
-import 'common/icon/iconfont'
-import './registerServiceWorker'
-import fastclick from 'fastclick'
-import VueLazyLoad from 'vue-lazyload'
-import VConsole from 'vconsole'
+import "babel-polyfill"
+import Vue from "vue"
+import "./cube-ui"
+import App from "./App.vue"
+import router from "./router"
+import store from "./vuex/store"
+import "common/scss/index.scss"
+import "common/icon/iconfont"
+import "./registerServiceWorker"
+import fastclick from "fastclick"
+import VueLazyLoad from "vue-lazyload"
+import VConsole from "vconsole"
 
 fastclick.attach(document.body) // 解决移动端点击300ms延迟
 
 Vue.use(VueLazyLoad, {
-  loading: require('common/imgs/default.png')
+  loading: require("common/imgs/default.png"),
 })
 
 /* eslint-disable no-unused-vars */
-var vConsole = new VConsole();
+// var vConsole = new VConsole()
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app")
